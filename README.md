c89b64fe662cd6cb9a9b89439414f4a040d19b53
AnhPmcl2015 <phananh.pmcl2015@gmail.com>
2018-10-07 15:16:30 +0700

    Vocabulary Template

--- README.md
+++
@@ -1,4 +0,0 @@
-# EnglishWeb
-
-English for Kids (Vocabulary)
-subject: Web Development and Application
---
+++ js/vocabulary.js
@@ -0,0 +1,20 @@
+function textToSpeech(text, callback) {
+    alert("Hello");
+    var u = new SpeechSynthesisUtterance();
+    u.text = text;
+    u.lang = 'en-US';
+
+    u.onend = function () {
+        if (callback) {
+            callback();
+        }
+    };
+
+    u.onerror = function (e) {
+        if (callback) {
+            callback(e);
+        }
+    };
+
+    speechSynthesis.speak(u);
+}
